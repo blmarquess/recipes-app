b686c9240ce558f0013dddc406e3c8d46c3a69e3
Antonio Martins <anmmartyns@gmail.com>
2022-01-27 17:46:58 -0300

    Cria CSS global da aplicacao

---
+++ src/components/assets/global.js
@@ -0,0 +1,16 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`* {
+    box-sizing: border-box;
+    margin: 0;
+    outline: 0;
+    padding: 0;
+  }
+  
+  .loginView {
+    display: grid;
+  }
+
+  `;
+
+export default GlobalStyle;
