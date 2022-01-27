bebf6ab0a5e01216193a5929052daf13ff7d1c7b
Antonio Martins <anmmartyns@gmail.com>
2022-01-27 19:28:20 -0300

    Iniciando o requisito 9

---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+// import React from 'react';
+// import { profileIcon,  } from '../src/images/profileIcon.svg';
+
+// export default function Header() {
+//   return (
+//     <div>
+//       <Lin
+//         type="button"
+//         data-testid="profile-top-btn"
+//         id="top-button"
+//       >
+//         profileIcon
+//       </Lin>
+
+//     </div>);
+// }
