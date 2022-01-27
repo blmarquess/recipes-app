0605f4cf286cf5b275c42d367aa43cfa4e586af6
Antonio Martins <anmmartyns@gmail.com>
2022-01-27 18:57:39 -0300

    Incluir a rota /foods.
    Co-authored-by: Thauler <Thauler@users.noreply.github.com>
    Co-authored-by: JoaoVictorA <Joao-V-Avelino@users.noreply.github.com>
    Co-authored-by: Bruno Marques <blmarquess@users.noreply.github.com>
    Co-authored-by: Matheus Moura Oliveira <matheusmoura0@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import { Route, Switch } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Login from './views/Login';
+import Home from './views/Home';
 
 function App() {
   return (
     <Switch>
       <Route exact path="/" component={ Login } />
-      {/* <Route exact path="/foods" component={ Foods } />
+      <Route exact path="/foods" component={ Home } />
+      {/*
       <Route exact path="/drinks" component={ Drinks } />
       <Route exact path="/foods/:id" component={ FoodsDetails } />
       <Route exact path="/drinks/:id" component={ DrinksDetails } />
