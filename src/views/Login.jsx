240baa0314a4d0624d2ad9cd2f91191de56ca5ac
Antonio Martins <anmmartyns@gmail.com>
2022-01-27 17:47:59 -0300

    Cria pagina de login com validacao dos inputs.

--- src/views/Login.jsx
+++ src/views/Login.jsx
@@ -1,12 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Input from '../components/assets/Input';
+import ButtonSD from '../components/assets/ButtonSD';
 
-export default function Login() {
+const Login = () => {
+  const [loginState, setInfLogin] = useState({ user: '', psw: '' });
+  const stateUpdate = (e) => setInfLogin({ ...loginState, [e.name]: e.value });
+  const PSW_MIN = 6;
+  const dotCom = /^[a-z0-9._-]+@[a-z0-9]+\.com$/;
+  const isValidForm = () => loginState.psw.length >= PSW_MIN
+    && dotCom.test(loginState.user);
+
   return (
-    <div>
+    <div className="loginView">
       <span>Login:</span>
-      <Input wsize="200px" />
+      <Input
+        type="email"
+        name="user"
+        data-testid="email-input"
+        wsize="200px"
+        value={ loginState.user }
+        onChange={ ({ target }) => stateUpdate(target) }
+      />
       <span>Senha:</span>
-      <Input wsize="200px" />
-    </div>);
-}
+      <Input
+        value={ loginState.psw }
+        type="password"
+        name="psw"
+        data-testid="password-input"
+        wsize="200px"
+        onChange={ ({ target }) => stateUpdate(target) }
+      />
+      <ButtonSD
+        data-testid="login-submit-btn"
+        disabled={ !isValidForm() }
+      >
+        Entrar
+      </ButtonSD>
+    </div>
+  );
+};
+
+export default Login;
