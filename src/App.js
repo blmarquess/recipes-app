cfc7dbc6510e893e6037281bfe2c47cd813fc073
Antonio Martins <anmmartyns@gmail.com>
2022-01-27 16:53:28 -0300

    Setup do Redux, rotas e criado o componente Login

--- src/App.js
+++ src/App.js
@@ -1,20 +1,28 @@
 import React from 'react';
 import './App.css';
-import rockGlass from './images/rockGlass.svg';
+import { Route, Switch } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Login from './views/Login';
 
 function App() {
   return (
-    <div className="meals">
-      <span className="logo">TRYBE</span>
-      <object
-        className="rocksGlass"
-        type="image/svg+xml"
-        data={ rockGlass }
-      >
-        Glass
-      </object>
-    </div>
+    <Switch>
+      <Route exact path="/" component={ Login } />
+      {/* <Route exact path="/foods" component={ Foods } />
+      <Route exact path="/drinks" component={ Drinks } />
+      <Route exact path="/foods/:id" component={ FoodsDetails } />
+      <Route exact path="/drinks/:id" component={ DrinksDetails } />
+      <Route exact path="/foods/:id/in-progress" component={ FoodRecipies } />
+      <Route exact path="/drnks/:id/in-progress" component={ DrinksRecipies } />
+      <Route exact path="/explore" component={ Explore } />
+      <Route exact path="/explore/foods" component={ ExploreFoods } />
+      <Route exact path="/explore/drinks" component={ ExploreDrinks } />
+      <Route exact path="/explore/drinks/ingredients" component={ DrinksIngredients } />
+      <Route exact path="/explore/foods/nationalities" component={ FoodsNationalities } />
+      <Route exact path="/profile" component={ Profile } />
+      <Route exact path="/done-recipes" component={ DoneRecipes } />
+      <Route exact path="/favorite-recipes" component={ FavoriteRecipes } /> */}
+    </Switch>
   );
 }
 
