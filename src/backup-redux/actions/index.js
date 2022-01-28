60a9f606457b0c8b69bc309ff17705e872d0e810
Bruno <blmarques.dev@gmail.com>
2022-01-28 20:27:12 -0300

    Clear
    Co-authored-by: Matheus Moura Oliveira <matheusmoura0@users.noreply.github.com>
    Co-authored-by: Thauler <Thauler@users.noreply.github.com>
    Co-authored-by: JoaoVictorA <Joao-V-Avelino@users.noreply.github.com>
    Co-authored-by: AntonioMartinsNt <AntonioMartinsNt@users.noreply.github.com>
    Co-authored-by: AirtonL <AirtonL@users.noreply.github.com>

--- src/backup-redux/actions/index.js
+++
@@ -1,6 +0,0 @@
-export const SAVE_LOGIN = 'SAVE_LOGIN';
-
-export const userAction = (email) => ({
-  type: SAVE_LOGIN,
-  email,
-});
--- src/backup-redux/redux/reducers/index.js
+++
@@ -1,7 +0,0 @@
-import { combineReducers } from 'redux';
-import login from './login';
-import display from './display';
-
-const rootReducer = combineReducers({ login, display });
-
-export default rootReducer;
