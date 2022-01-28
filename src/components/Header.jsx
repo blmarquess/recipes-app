b989fb15dbaa2e2ece172be7f3c1cae03643edb7
Bruno <blmarques.dev@gmail.com>
2022-01-28 16:26:10 -0300

    Feat: Make header btn search opt
    Co-authored-by: Matheus Moura Oliveira <matheusmoura0@users.noreply.github.com>
    Co-authored-by: Thauler <Thauler@users.noreply.github.com>

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,38 @@
-// import React from 'react';
-// import { profileIcon,  } from '../src/images/profileIcon.svg';
+import React from 'react';
+import { useDispatch } from 'react-redux';
+import profileIcon from '../images/profileIcon.svg';
+import searchIcon from '../images/searchIcon.svg';
+import { useHistory } from 'react-router-dom';
+import { searchSW } from '../redux/actions/'
 
-// export default function Header() {
-//   return (
-//     <div>
-//       <Lin
-//         type="button"
-//         data-testid="profile-top-btn"
-//         id="top-button"
-//       >
-//         profileIcon
-//       </Lin>
+export default function Header() {
+  const history = useHistory();
+  const dispatch = useDispatch();
 
-//     </div>);
-// }
+  function handleClick() {
+    history.push("/profile");
+  }
+
+  return (
+    <div clasname="header-app">
+      <button
+        type="button"
+        data-testid="profile-top-btn"
+        onClick={ () => handleClick() }
+      >
+        <img src={ profileIcon } />
+      </button>
+      <h2 data-testid="page-title"> 
+      Page Title    
+      </h2>
+      <button
+        type="button"
+        data-testid="search-top-btn"
+        onClick={ () => dispatch(searchSW())}
+      >
+        <img src={ searchIcon } />
+      </button>
+      
+
+    </div>);
+}
