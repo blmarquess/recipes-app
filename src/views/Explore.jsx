8ddf84b99eb7f98ab2c7da90297feb9b639ef60a
Bruno <blmarques.dev@gmail.com>
2022-01-28 16:37:51 -0300

    Feat: page Explore/esploreDrinks
    Co-authored-by: Matheus Moura Oliveira <matheusmoura0@users.noreply.github.com>
    Co-authored-by: Thauler <Thauler@users.noreply.github.com>

---
+++ src/views/Explore.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import Header from '../components/Header';
+
+export default function Explore() {
+  return <div>
+    <Header />
+    Explore recipes
+  </div>;
+}
---
+++ src/views/ExploreDrinks.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import Header from '../components/Header';
+
+export default function ExploreDrinks() {
+  return <div>
+    <Header />
+    Explore Drinks
+  </div>;
+}
