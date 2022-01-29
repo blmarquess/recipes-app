3323c71b977656d4ed00995fe80c65e23a6e4391
Bruno <blmarques.dev@gmail.com>
2022-01-29 17:34:17 -0300

    Feat: Explore Foods details
    Co-authored-by: Matheus Moura Oliveira <matheusmoura0@users.noreply.github.com>
    Co-authored-by: Thauler <Thauler@users.noreply.github.com>

--- src/views/ExploreFoods.jsx
+++ src/views/ExploreFoods.jsx
@@ -1,6 +1,37 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import HeaderProfile from '../components/HeaderNoSer';
+import Footer from '../components/Footer';
 
 export default function ExploreFoods() {
-  return (<HeaderProfile title="Explore Foods" />);
+  return (
+    <div>
+      <HeaderProfile title="Explore Foods" />
+      <Link to="./explore/foods/ingredients">
+        <button
+          data-testid="explore-by-ingredient"
+          type="button"
+        >
+          By Ingridient
+        </button>
+      </Link>
+      <Link to="./explore/foods/nationalities">
+        <button
+          data-testid="explore-by-nationality"
+          type="button"
+        >
+          By Nationality
+        </button>
+      </Link>
+      <Link to="/foods/random">
+        <button
+          data-testid="explore-surprise"
+          type="button"
+        >
+          Surprise Me!
+        </button>
+      </Link>
+      <Footer />
+    </div>
+  );
 }
