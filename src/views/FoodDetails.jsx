aa12d37e33f5e3c7b5ae64d7d6cd0ef4051a479b
Bruno <blmarques.dev@gmail.com>
2022-01-29 17:36:51 -0300

    Feat: DIDMount whit getAPI infos for id recebido por props
    Co-authored-by: Matheus Moura Oliveira <matheusmoura0@users.noreply.github.com>
    Co-authored-by: Thauler <Thauler@users.noreply.github.com>

--- src/views/FoodDetails.jsx
+++ src/views/FoodDetails.jsx
@@ -1,9 +1,33 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+import { getDataApiMeals } from '../utils/tools';
 
 export default function FoodDetails() {
+  const [randomId, setRandomId] = useState([{ idMeal: 'loading...' }]);
+  const pathId = useLocation().pathname.split('/foods/')[1];
+
+  const randomMeal = async () => {
+    const mealId = await getDataApiMeals('random');
+    return mealId;
+  };
+
+  useEffect(() => {
+    console.log('effectssss"');
+    const setDataState = async () => {
+      if (pathId === 'random') {
+        console.log('caiu no if');
+        return randomMeal().then((res) => setRandomId(res.meals));
+      }
+    };
+    setDataState();
+    return () => { };
+  }, [pathId]);
+
+  console.log(randomId);
+
   return (
     <div>
-      Food Details
+      {randomId.map((meal) => <p key={ Math.random() }>{ meal.idMeal }</p>)}
     </div>
   );
 }
