629814a084913a0c5f1a92343f22bd3a604d53ce
Bruno <blmarques.dev@gmail.com>
2022-01-29 17:34:57 -0300

    Feat: Include footer on component
    Co-authored-by: Matheus Moura Oliveira <matheusmoura0@users.noreply.github.com>
    Co-authored-by: Thauler <Thauler@users.noreply.github.com>

--- src/views/FoodRecipies.jsx
+++ src/views/FoodRecipies.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import HeaderProfile from '../components/HeaderNoSer';
+import Footer from '../components/Footer';
 
 export default function FoodRecipies() {
   return (
     <div>
       <HeaderProfile title="FoodRecipies" />
       <h2>teste2</h2>
+      <Footer />
     </div>
   );
 }
