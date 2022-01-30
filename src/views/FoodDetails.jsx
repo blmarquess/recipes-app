47c0e743a7a7ca361bbf956a4fb579bd8d8a00c8
Bruno <blmarques.dev@gmail.com>
2022-01-30 00:58:23 -0300

    Feat: make page details of drinks

--- src/views/FoodDetails.jsx
+++ src/views/FoodDetails.jsx
@@ -3,7 +3,7 @@ import { useLocation } from 'react-router-dom';
 import { getDataApiMeals } from '../utils/tools';
 
 export default function FoodDetails() {
-  const [randomId, setRandomId] = useState([{ idMeal: 'loading...' }]);
+  const [mealState, setRandomId] = useState([]);
   const pathId = useLocation().pathname.split('/foods/')[1];
 
   const randomMeal = async () => {
@@ -23,11 +23,41 @@ export default function FoodDetails() {
     setDataState();
   }, [pathId]);
 
-  console.log(randomId);
-
   return (
     <div>
-      {randomId.map((meal) => <p key={ Math.random() }>{ meal.idMeal }</p>)}
+      {mealState.map((meal) => (
+        <div
+          key={ Math.random() }
+        >
+          <img src={ meal.strMealThumb } alt={ meal.strMeal } />
+          <h2>{ meal.strMeal }</h2>
+          <h6>{ meal.strCategory }</h6>
+        </div>
+      ))}
+      <h3>Ingredients</h3>
+      {/* {mealState
+        .filter((mealIgr, i) => (`${mealIgr.strIngredient}${i}` !== null || ''
+        ? (<span>{`${mealIgr.strIngredient}${i}`}</span>) : null
+      ))} */}
+      <h3>Instructions</h3>
+      {mealState.map((mealInst) => (
+        <p key={ Math.random() }>
+          {mealInst.strInstructions}
+        </p>))}
+      <h3>Vídeo</h3>
+      {mealState.length > 0
+      && <iframe
+        src={ `https://www.youtube.com/embed/${mealState[0].strYoutube.split('v=')[1]}` }
+        frameBorder="0"
+        allowFullScreen
+        title="video"
+        data-testid="video"
+      />}
+      <button
+        type="button"
+      >
+        Start Recipe
+      </button>
     </div>
   );
 }
