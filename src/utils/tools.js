8add8c805fb91b5f82aa9c9548598a0402b91ee1
Bruno <blmarques.dev@gmail.com>
2022-01-30 01:00:59 -0300

    Feat: dispatch api requesto on search bar
    Co-authored-by: Thauler <Thauler@users.noreply.github.com>
    Co-authored-by: Matheus Moura Oliveira <matheusmoura0@users.noreply.github.com>

--- src/utils/tools.js
+++ src/utils/tools.js
@@ -1,5 +1,3 @@
-import axios from 'axios';
-
 export const readLocalData = (data) => JSON.parse(localStorage.getItem(data));
 export const saveLocalData = (keyDB, object) => localStorage
   .setItem(keyDB, JSON.stringify(object));
@@ -8,6 +6,7 @@ export const API_MEAL = {
   ingredient: 'https://www.themealdb.com/api/json/v1/1/filter.php?i=',
   nome: 'https://www.themealdb.com/api/json/v1/1/search.php?s=',
   random: 'https://www.themealdb.com/api/json/v1/1/random.php',
+  firstletter: 'https://www.themealdb.com/api/json/v1/1/search.php?f=',
   id: 'https://www.themealdb.com/api/json/v1/1/lookup.php?i=',
 };
 
@@ -19,15 +18,14 @@ export const API_DRINKS = {
   id: 'https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=',
 };
 
-export const getDataApiDrinks = async (destination, query = '') => {
-  const data = await axios.get(`${API_DRINKS[destination]}${query}`)
-    .then((response) => response.data);
+export const getDataApiDrinks = async (destination, query) => {
+  const data = await fetch(`${API_DRINKS[destination]}${query}`)
+    .then((response) => response.json());
   return data;
 };
 
-export const getDataApiMeals = async (destination, query = '') => {
-  console.log(`${API_MEAL[destination]}${query}`);
-  const data = await axios.get(`${API_MEAL[destination]}${query}`)
-    .then((response) => response.data);
+export const getDataApiMeals = async (destination, query) => {
+  const data = await fetch(`${API_MEAL[destination]}${query}`)
+    .then((response) => response.json());
   return data;
 };
