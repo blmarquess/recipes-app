de5522dc6711edad4b50df7dc6257960a97ad2cc
Bruno <blmarques.dev@gmail.com>
2022-01-31 18:17:59 -0300

    Feat: Profile page
    Co-authored-by: JoaoVictorA <Joao-V-Avelino@users.noreply.github.com>
    Co-authored-by: Matheus Moura Oliveira <matheusmoura0@users.noreply.github.com>
    Co-authored-by: Thauler <Thauler@users.noreply.github.com>

--- src/views/Profile.jsx
+++ src/views/Profile.jsx
@@ -1,11 +1,39 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import HeaderNoSer from '../components/HeaderNoSer';
 import Footer from '../components/Footer';
+import { readLocalData, dataCleaner } from '../utils/tools';
 
 export default function Profile() {
+  const Email = readLocalData('user');
   return (
     <div>
       <HeaderNoSer title="Profile" />
+      <h2
+        data-testid="profile-email"
+      >
+        {Email.email}
+      </h2>
+      <Link to="/done-recipes">
+        <button type="button" data-testid="profile-done-btn">Done receipes</button>
+      </Link>
+      <Link to="/favorite-recipes">
+        <button
+          type="button"
+          data-testid="profile-favorite-btn"
+        >
+          Favorite receipes
+        </button>
+      </Link>
+      <Link to="/">
+        <button
+          onClick={ dataCleaner }
+          type="button"
+          data-testid="profile-logout-btn"
+        >
+          Logout
+        </button>
+      </Link>
       <Footer />
     </div>
   );
