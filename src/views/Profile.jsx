43257fef0e2fcdf7fb4cfe8b2beedf9bee45332b
Bruno <blmarques.dev@gmail.com>
2022-02-01 00:21:52 -0300

    Feat: Get user from Redux

--- src/views/Profile.jsx
+++ src/views/Profile.jsx
@@ -1,18 +1,21 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import HeaderNoSer from '../components/HeaderNoSer';
 import Footer from '../components/Footer';
-import { readLocalData, dataCleaner } from '../utils/tools';
+// import { readLocalData, dataCleaner } from '../utils/tools';
+import { dataCleaner } from '../utils/tools';
 
 export default function Profile() {
-  const Email = readLocalData('user');
+  const user = useSelector((state) => state.login);
+  // const Email = readLocalData('user');
   return (
     <div>
       <HeaderNoSer title="Profile" />
       <h2
         data-testid="profile-email"
       >
-        {Email.email}
+        {user.email}
       </h2>
       <Link to="/done-recipes">
         <button type="button" data-testid="profile-done-btn">Done Recipes</button>
