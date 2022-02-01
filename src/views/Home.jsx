62f3e0d44fa96f55d169e4095b12ceeaaa985cb3
Bruno <blmarques.dev@gmail.com>
2022-02-01 04:16:51 -0300

    Feat: Dynamic render DisplayCards and usePageTitle

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,14 +1,19 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 import Layout from '../components/assets/layout';
-
-const title = 'Foods';
+import DisplayCards from '../components/DisplayCards';
+import usePageTitle from '../utils/usePageTitle';
 
 export default function Home() {
+  const state = useSelector((store) => store.searchdata);
+  const pageTitle = usePageTitle();
+
   return (
     <Layout>
-      <Header title={ title } />
+      <Header title={ pageTitle } />
+      { state.data.length > 1 && <DisplayCards />}
       <Footer />
     </Layout>);
 }
