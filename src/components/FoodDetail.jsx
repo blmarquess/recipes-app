e41a972c2b38f5a6b5af358a6805ab8c3d82f13a
Bruno <blmarques.dev@gmail.com>
2022-01-31 22:13:14 -0300

    Feat: component para renderizar os detalhes das receitas
    Co-authored-by: JoaoVictorA <Joao-V-Avelino@users.noreply.github.com>
    Co-authored-by: Matheus Moura Oliveira <matheusmoura0@users.noreply.github.com>
    Co-authored-by: Thauler <Thauler@users.noreply.github.com>

---
+++ src/components/FoodDetail.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+
+export default function FoodDetail() {
+  // const [mealState, setRandomId] = useState([]);
+
+  return (
+    <div>
+      {mealState.map((meal) => (
+        <div
+          key={ Math.random() }
+        >
+          <img src={ meal.strMealThumb } alt={ meal.strMeal } />
+          <h2>{ meal.strMeal }</h2>
+          <h6>{ meal.strCategory }</h6>
+        </div>
+      ))}
+      <h3>Ingredients</h3>
+      {/* {mealState
+        .filter((mealIgr, i) => (`${mealIgr.strIngredient}${i}` !== null || ''
+        ? (<span>{`${mealIgr.strIngredient}${i}`}</span>) : null
+      ))} */}
+      <h3>Instructions</h3>
+      {mealState.map((mealInst) => (
+        <p key={ Math.random() }>
+          {mealInst.strInstructions}
+        </p>))}
+      <h3>Vídeo</h3>
+      {mealState.length > 0
+      && <iframe
+        src={ `https://www.youtube.com/embed/${mealState[0].strYoutube.split('v=')[1]}` }
+        frameBorder="0"
+        allowFullScreen
+        title="video"
+        data-testid="video"
+      />}
+      <button
+        type="button"
+      >
+        Start Recipe
+      </button>
+    </div>
+  );
+}
