1efe6584614bce1ecc2562d72e4556c4dc97cd43
Bruno <blmarques.dev@gmail.com>
2022-02-02 22:22:35 -0300

    Feat: Make component displays for drinks

--- src/components/DrinkDetails.jsx
+++ src/components/DrinkDetails.jsx
@@ -1,32 +1,42 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-export default function DrinkDetails() {
+export default function DrinkDetails(props) {
+  const {
+    strDrinkThumb,
+    strDrink,
+    strCategory,
+    strIngredient,
+    strInstructions,
+  } = props;
   return (
-    <div>
-      {drinksState.map((drink) => (
-        <div
-          key={ Math.random() }
-        >
-          <img src={ drink.strDrinkThumb } alt={ drink.strDrink } />
-          <h2>{ drink.strDrink }</h2>
-          <h6>{ drink.strCategory }</h6>
-        </div>
-      ))}
-      <h3>Ingredients</h3>
-      {/* {drinksState
-        .filter((drinkIgr, i) => (`${drinkIgr.strIngredient}${i}` !== null || ''
-        ? (<span>{`${drinkIgr.strIngredient}${i}`}</span>) : null
-      ))} */}
-      <h3>Instructions</h3>
-      {drinksState.map((drinkInst) => (
-        <p key={ Math.random() }>
-          {drinkInst.strInstructions}
-        </p>))}
+    <>
+      <section>
+        <img src={ strDrinkThumb } alt={ strDrink } />
+        <h2>{ strDrink }</h2>
+        <h6>{ strCategory }</h6>
+      </section>
+      <section>
+        <h3>Ingredients</h3>
+        <p>{ strIngredient }</p>
+      </section>
+      <section>
+        <h3>Instructions</h3>
+        <p>{ strInstructions }</p>
+      </section>
       <button
         type="button"
       >
         Start Recipe
       </button>
-    </div>
+    </>
   );
 }
+
+DrinkDetails.propTypes = {
+  strCategory: PropTypes.string,
+  strIngredient: PropTypes.string,
+  strInstructions: PropTypes.string,
+  strDrink: PropTypes.string,
+  strDrinkThumb: PropTypes.string,
+}.isRequired;
