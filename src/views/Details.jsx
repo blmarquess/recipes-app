17cbcd565d0e3925ba0f13f1b1c7e84cc7a3b9dc
Thauler <thauler98@gmail.com>
2022-02-03 18:30:56 -0300

    Feat: Change from redux to localstorage
    Co-authored-by: Bruno Marques <blmarquess@users.noreply.github.com>

--- src/views/Details.jsx
+++ src/views/Details.jsx
@@ -1,42 +1,49 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
-import { useLocation } from 'react-router-dom';
-// import React, { useEffect } from 'react';
-// import { useSelector, useDispatch } from 'react-redux';
-// import { useLocation, useParams } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { useLocation, useParams } from 'react-router-dom';
 import LayoutPage from '../components/assets/LayoutPage';
 import FoodDetail from '../components/FoodDetail';
 import DrinkDetails from '../components/DrinkDetails';
-// import { clearSingleID } from '../redux/actions';
+import { readLocalData, getDataApi, saveLocalData } from '../utils/tools';
 
 export default function Details() {
-  // const { id } = useParams();
+  const { id } = useParams();
   const rota = useLocation().pathname.replace('/', '').split('/')[0];
   const objSelector = rota.includes('drinks') ? 'drinks' : 'meals';
-  const data = useSelector((state) => state.searchdata);
-  const recipe = data.random;
-  const { isDone } = data;
-  // const dispatch = useDispatch();
+  const switchID = objSelector === 'drinks' ? 'idDrink' : 'idMeal';
+  const localData = readLocalData('DetailItem');
 
-  // useEffect(() => {
-  //   if (recipe[objSelector]) {
-  //     return console.log(recipe[objSelector]);
-  //   }
-  //   const setDataState = async () => getDataApi(rota, 'id', id).then((res) => {
-  //     dispatch(SearchRandomAPI(res));
-  //   });
+  const [detailState, setDetails] = useState([]);
 
-  //   if (data[objSelector][0]) {
-  //     setDataState();
-  //   }
-  // }, [id, rota, objSelector, data, dispatch, recipe]);
+  useEffect(() => {
+    const updateState = (itm) => setDetails(...itm[objSelector]);
+
+    const hasData = async () => {
+      if (localData === null) {
+        console.log('Primeiro IF!');
+        await getDataApi(rota, 'id', id).then((res) => {
+          updateState(res);
+          saveLocalData('DetailItem', res);
+        });
+        return console.log('');
+      }
+
+      if (detailState.length === 0) {
+        await getDataApi(rota, 'id', id).then((res) => {
+          updateState(res);
+          saveLocalData('DetailItem', res);
+        });
+        return console.log('');
+      }
+    };
+    hasData();
+  }, [objSelector, switchID, id, rota, localData, detailState]);
 
   return (
     <LayoutPage>
-      { isDone
+      { detailState.lenght > 0
         && rota === 'foods'
-        ? <FoodDetail { ...recipe[objSelector][0] } />
-        : <DrinkDetails { ...recipe[objSelector][0] } />}
+        ? <FoodDetail { ...detailState } />
+        : <DrinkDetails { ...detailState } />}
     </LayoutPage>
   );
 }
