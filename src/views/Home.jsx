a7fb7b8793d7fcaa7876fe14705aeff4f0592118
Bruno <blmarques.dev@gmail.com>
2022-02-05 20:55:47 -0300

    Feat: Refactore to useReducer and ContextAPI

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,52 +1,27 @@
-import React, { useEffect, useState } from 'react';
-import { useDispatch } from 'react-redux';
-import { useLocation } from 'react-router-dom';
+import React, { useContext } from 'react';
+
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 import LayoutPage from '../components/assets/LayoutPage';
 import DisplayCards from '../components/DisplayCards';
 // import DisplayCategory from '../components/DisplayCategory';
 import usePageTitle from '../utils/usePageTitle';
-import { getDataApi } from '../utils/tools';
-import { SearchDataAPI, SearchCategory } from '../redux/actions';
+import { DataRecipesContext } from '../context/store';
 
 export default function Home() {
-  const [recipeList, setList] = useState([]);
-  const [categoryList, setCategoryList] = useState([]);
-  const dispatch = useDispatch();
   const pageTitle = usePageTitle();
-  const rota = useLocation().pathname.replace('/', '').split('/')[0];
+  const recipesList = useContext(DataRecipesContext);
 
-  useEffect(() => {
-    const setDataState = (dados) => setList(dados);
-    const setDataCategory = (dados) => setCategoryList(dados);
-    console.log('<-- LoopEffects');
-    // console.log('receitas', recipeList, 'categorias', categoryList);
-
-    const initPage = async () => {
-      if (recipeList.length < 1) {
-        getDataApi(rota, 'all').then((response) => {
-          setDataState(response);
-          dispatch(SearchDataAPI(response));
-        });
-      }
-      if (categoryList.length < 1) {
-        getDataApi(rota, 'categorias').then((response) => {
-          setDataCategory(response);
-          dispatch(SearchCategory(response));
-        });
-      }
-    };
-    initPage();
-    // console.log('recipe', recipeList, 'category', categoryList);
-  }, [rota, recipeList, categoryList, dispatch]);
+  console.log(recipesList);
+  // const rota = useLocation().pathname.replace('/', '').split('/')[0];
 
   return (
     <LayoutPage>
       <Header title={ pageTitle } />
-      { console.log(recipeList.meals.length === 1) }
-      {recipeList.meals.length === 0 ? <DisplayCards { ...recipeList } /> : ''}
-      {/* {categoryList.length === 1 && <DisplayCategory { ...categoryList } />} */}
+
+      <DisplayCards />
+      {/* {categoryList.length === 1 && <DisplayCategory />} */}
+
       <Footer />
     </LayoutPage>);
 }
