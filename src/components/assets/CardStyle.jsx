d314bb93a898217ad2b8568adf4659ca2836f359
Bruno <blmarques.dev@gmail.com>
2022-02-06 20:16:10 -0300

    Feat: CardStyled component

---
+++ src/components/assets/CardStyle.jsx
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+const CardS = styled.section`align-items: center;
+  background: rgba(255, 254, 254, 0.938);
+  border: solid 1.5px rgba(235, 235, 235, 0.753);
+  border-radius: 0.4rem;
+  box-shadow: 0 7px 11px -10px rgba(26, 24, 24, 0.815);
+  display: flex;
+  flex-direction: column;
+  height: 170px;
+  justify-content: space-around;
+  margin-bottom: 5px;
+  padding: 0 0 0.3rem;
+  width: 140px;
+`;
+
+export default CardS;
