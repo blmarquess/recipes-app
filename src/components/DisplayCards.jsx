bf029c78bf0a658f4e28d69121b8f7a8545ca9fb
Bruno <blmarques.dev@gmail.com>
2022-02-06 20:15:51 -0300

    Feat: Eventlistener Click to redirect in cards

--- src/components/DisplayCards.jsx
+++ src/components/DisplayCards.jsx
@@ -1,8 +1,8 @@
 import React, { useContext, useEffect } from 'react';
 import PropTypes from 'prop-types';
-import { useLocation } from 'react-router-dom';
+import { useLocation, useHistory } from 'react-router-dom';
 import { StoreContext, DispatchContext } from '../context/store';
-import { recipesListAPI } from '../context/action';
+import { recipeInFoco, recipesListAPI } from '../context/action';
 
 import Card from './assets/Card';
 
@@ -10,6 +10,7 @@ import { getDataApi } from '../utils/tools';
 
 export default function DisplayCards() {
   const rota = useLocation();
+  const history = useHistory();
   const inMeals = rota.pathname.includes('foods');
   const inDrinks = rota.pathname.includes('drinks');
   const store = useContext(StoreContext);
@@ -23,39 +24,67 @@ export default function DisplayCards() {
         });
         return true;
       }
+      if (inMeals && store.recipeslist.drinks) {
+        await getDataApi('foods', 'all').then((res) => {
+          dispatch(recipesListAPI(res));
+        });
+        return true;
+      }
     };
     getDrinks();
-  }, [inDrinks, dispatch, rota, store.recipeslist.meals]);
+  }, [inDrinks, inMeals, dispatch, store.recipeslist.meals, store.recipeslist.drinks]);
+
+  const pushTo = (keyID) => {
+    if (inDrinks) {
+      const recipDri = store.recipeslist.drinks.find(({ idDrink }) => idDrink === keyID);
+      dispatch(recipeInFoco(recipDri));
+      return history.push(`/drinks/${keyID}`);
+    }
+    if (inMeals) {
+      const recipMeal = store.recipeslist.meals.find(({ idMeal }) => idMeal === keyID);
+      dispatch(recipeInFoco(recipMeal));
+      return history.push(`/foods/${keyID}`);
+    }
+  };
 
   return (
     <>
       <button type="button" onClick={ () => console.log(store) }>
         test log
       </button>
+      {/* utilisação de evento de click em elementos não button https://dev.to/receter/easy-accessible-click-handlers-4jkb */}
       <section
         className="displayCard"
         role="button"
         tabIndex={ 0 }
-        onKeyDown={ () => console.log(dispatch) }
-        onClick={ () => console.log('clicou') }
+        onKeyDown={ () => true }
+        onClick={ ({ target }) => pushTo(target.attributes['data-id'].value) }
       >
         {inMeals
           && store.recipeslist.meals
           && store.recipeslist.meals.slice(0, +'12').map((item, index) => (
             <Card
               key={ item.idMeal }
+              keyItem={ item.idMeal }
               index={ index }
               srcName={ item.strMeal }
               imgSRC={ item.strMealThumb }
             />))}
       </section>
 
-      <section className="displayCard">
+      <section
+        className="displayCard"
+        role="button"
+        tabIndex={ 0 }
+        onKeyDown={ () => true }
+        onClick={ ({ target }) => pushTo(target.attributes['data-id'].value) }
+      >
         {inDrinks
           && store.recipeslist.drinks
           && store.recipeslist.drinks.slice(0, +'12').map((item, index) => (
             <Card
               key={ item.idDrink }
+              keyItem={ item.idDrink }
               index={ index }
               srcName={ item.strDrink }
               imgSRC={ item.strDrinkThumb }
