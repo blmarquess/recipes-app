1a538b250bca818188e5d3e49df8c5bd7b58739a
Bruno <blmarques.dev@gmail.com>
2022-02-06 20:10:54 -0300

    Feat: add reducer for categorys and FocusToDetailsItem

--- src/context/action.js
+++ src/context/action.js
@@ -1,9 +1,17 @@
+import { clearFocusItem } from '../utils/tools';
+
 export const IN_FOCO = 'IN_FOCO';
 export const RECIPES_LIST = 'RECIPES_LIST';
+export const CATEGORY_LIST = 'CATEGORY_LIST';
 export const LOGIN = 'LOGIN';
 
 export const login = (payload) => ({ type: LOGIN, payload });
 
-export const recipeInFoco = (payload) => ({ type: IN_FOCO, payload });
-
 export const recipesListAPI = (payload) => ({ type: RECIPES_LIST, payload });
+
+export const recipesCategory = (payload) => ({ type: CATEGORY_LIST, payload });
+
+export const recipeInFoco = (payload) => {
+  clearFocusItem();
+  return ({ type: IN_FOCO, payload });
+};
