9a23ca5d9b2d217ba8326338d51416387849c11e
Bruno <blmarques.dev@gmail.com>
2022-02-07 05:33:17 -0300

    Fix: width page to 360px

--- src/components/assets/LayoutPage.jsx
+++ src/components/assets/LayoutPage.jsx
@@ -1,14 +1,22 @@
 import styled from 'styled-components';
 
-const LayoutPage = styled.section`background: rgba(255, 254, 254, 0.83);
+const LayoutPage = styled.section`align-items: center;
+  background: rgba(255, 254, 254, 0.83);
   display: flex;
+  flex-direction: column;
+  height: 100%;
+  justify-content: center;
+  margin: 0 auto;
+  max-width: 360;
+  padding: 0 0.5rem;
+  width: 360;
 
   @media ( min-width : 200px ) and ( max-width : 1600px ) {
+    align-items: space-evenly;
     display: flex;
     flex-direction: column;
     height: 100%;
-    margin: 0 auto;
-    width: 360;
+    justify-content: space-around;
   }
 `;
 
