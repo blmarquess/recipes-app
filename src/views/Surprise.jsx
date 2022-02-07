8f68dec630d4cc24315edc27c5bcdd9e95aac6f6
Bruno <blmarques.dev@gmail.com>
2022-02-06 21:01:44 -0300

    Migrate(surpriseme) refactory to useReducer

--- src/views/Surprise.jsx
+++ src/views/Surprise.jsx
@@ -1,19 +1,20 @@
-import React from 'react';
-// import { useHistory, useParams } from 'react-router-dom';
-// import { getDataApi, redirectToID } from '../utils/tools';
-// import { SearchRandomAPI } from '../redux/actions';
+import React, { useContext, useEffect } from 'react';
+import { useHistory, useParams } from 'react-router-dom';
+import { getDataApi, redirectToID } from '../utils/tools';
+import { recipeInFoco } from '../context/action';
+import { DispatchContext } from '../context/store';
 
 export default function Surprise() {
-  // const { id } = useParams();
-  // const dispatch = useDispatch();
-  // const history = useHistory();
+  const { id } = useParams();
+  const dispatch = useContext(DispatchContext);
+  const history = useHistory();
 
-  // useEffect(() => {
-  //   const setDataState = async () => getDataApi(id, 'random', '').then((res) => {
-  //     dispatch(SearchRandomAPI(res));
-  //     const toGo = redirectToID(res);
-  //     return history.push(toGo);
-  //   }); setDataState();
-  // }, [id, dispatch, history]);
+  useEffect(() => {
+    const setDataState = async () => getDataApi(id, 'random', '').then((res) => {
+      dispatch(recipeInFoco(res));
+      const toGo = redirectToID(res);
+      return history.push(toGo);
+    }); setDataState();
+  }, [id, dispatch, history]);
   return (<div />);
 }
