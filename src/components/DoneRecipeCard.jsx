79df044de6466a7a4b112eb493c2daf720e0a181
Bruno Marques <44172179+blmarquess@users.noreply.github.com>
2022-02-09 15:56:19 -0300

    Feat: Page of done recipes
    Co-authored-by: AntonioMartinsNt <AntonioMartinsNt@users.noreply.github.com>

---
+++ src/components/DoneRecipeCard.jsx
@@ -0,0 +1,88 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import shareIcon from '../images/shareIcon.svg';
+// import blackHeartIcon from '../images/blackHeartIcon.svg';
+import AlertLC from './assets/AlertLC';
+
+const copy = require('clipboard-copy');
+
+export default function DoneRecipeCard(props) {
+  // const { index, strMealThumb, strCategory, strMeal, strDrinkThumb, strTags } = props;
+  const {
+    index,
+    id,
+    type,
+    nationality,
+    category,
+    alcoholicOrNot,
+    name,
+    image,
+    doneDate,
+    tags } = props;
+  const ONE_SECOND = 1000;
+  const [alertLC, setAlertLC] = useState(false);
+
+  // function splitStrTags() {
+  //   if (strTags !== null) {
+  //     const arrayStrTags = strTags.split(',');
+  //     return (
+  //       arrayStrTags
+  //         .map((tag) => (
+  //           <span key={ tag } data-testid={ `${index}-${tag}-horizontal-tag` }>
+  //             { tag }
+  //           </span>))
+  //     );
+  //   }
+  // }
+
+  function copyToClipboard() {
+    copy(`http://localhost:3000/${type}s/${id}`);
+    setAlertLC(true);
+    setTimeout(() => {
+      setAlertLC(false);
+    }, ONE_SECOND);
+  }
+
+  return (
+    <div>
+      <AlertLC alertLC={ alertLC } />
+      <Link to={ `/${type}s/${id}` }>
+        <img
+          width="200px"
+          data-testid={ `${index}-horizontal-image` }
+          src={ image }
+          alt=""
+        />
+        <h3 data-testid={ `${index}-horizontal-top-text` }>
+          { `${nationality} - ${category}` }
+        </h3>
+        <h3 data-testid={ `${index}-horizontal-top-text` }>{ alcoholicOrNot }</h3>
+        <h2 data-testid={ `${index}-horizontal-name` }>{ name }</h2>
+      </Link>
+      <h3 data-testid={ `${index}-horizontal-done-date` }>{ doneDate }</h3>
+      <button
+        type="button"
+        onClick={ copyToClipboard }
+      >
+        <img
+          data-testid={ `${index}-horizontal-share-btn` }
+          src={ shareIcon }
+          alt=""
+        />
+      </button>
+      { tags.map((tag) => (
+        <span key={ tag } data-testid={ `${index}-${tag}-horizontal-tag` }>{ tag }</span>
+      ))}
+    </div>
+  );
+}
+
+DoneRecipeCard.propTypes = {
+  index: PropTypes.string,
+  strCategory: PropTypes.string,
+  strIngredient: PropTypes.string,
+  strInstructions: PropTypes.string,
+  strDrink: PropTypes.string,
+  strDrinkThumb: PropTypes.string,
+}.isRequired;
