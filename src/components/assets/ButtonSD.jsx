635503c37ec68e1901f3bfd74641170dbf7cd3cc
Bruno Marques <44172179+blmarquess@users.noreply.github.com>
2022-02-09 04:29:56 -0300

    Feat: make props to sdButton

--- src/components/assets/ButtonSD.jsx
+++ src/components/assets/ButtonSD.jsx
@@ -4,15 +4,20 @@ const ButtonSD = styled.button`align-self: center;
   background: ${({ bgcolor }) => bgcolor || 'rgba(255, 14, 14, 0.753)'};
   border: solid 1px rgb(211, 219, 213);
   border-radius: 0.3rem;
+  bottom: ${(props) => props.bottom || '0'};
   color: ${({ textcolor }) => textcolor || '#fafaf9'};
   cursor: pointer;
   font-size: ${({ fontesize }) => fontesize || '22px'};
   font-weight: 600;
   height: 40px;
+  left: ${(props) => props.left || 'none'};
   margin: ${(props) => props.msize || '0'};
   outline: none;
   padding: 0 1rem;
+  position: ${(props) => props.position || 'relative'};
+  right: ${(props) => props.right || 'none'};
   text-align: center;
+  top: ${(props) => props.top || 'none'};
   width: ${(props) => props.wsize || 'auto'};
 
   &:disabled {
