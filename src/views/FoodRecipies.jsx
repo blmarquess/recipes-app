9a92c13db77fbb6ff99277512ca6c4ff58b747e9
Bruno Marques <44172179+blmarquess@users.noreply.github.com>
2022-02-09 23:44:15 -0300

    Feat: Component for render recipes in progress

--- src/views/FoodRecipies.jsx
+++ src/views/FoodRecipies.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
-import HeaderNoSer from '../components/HeaderNoSer';
+import { useLocation } from 'react-router-dom';
+import Header from '../components/Header';
 import Footer from '../components/Footer';
+import RecipesProgress from '../components/RecipesProgress';
 
 export default function FoodRecipies() {
+  const id = useLocation().pathname;
+  const rota = id.split('/')[1];
+  const recipeId = id.split('/')[2];
   return (
     <div>
-      <HeaderNoSer title="FoodRecipies" />
-      <h2>teste2</h2>
+      <Header title="DrinksRecipies" />
+      <RecipesProgress id={ recipeId } rota={ rota } />
+      <h2>teste</h2>
       <Footer />
     </div>
   );
