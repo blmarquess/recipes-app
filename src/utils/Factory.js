84f55191f225c6c62287201aeaef7e5247bafaa2
Bruno Marques <44172179+blmarquess@users.noreply.github.com>
2022-02-12 01:09:11 -0300

    Feat: Add recipieFavotiteFactory

---
+++ src/utils/Factory.js
@@ -0,0 +1,36 @@
+export function recipeIngredientsFactory(obAPI) {
+  const ingrdientLists = Object
+    .entries(obAPI).filter((key) => key[0]
+      .includes('strIngredient'))
+    .map((recipe) => recipe[1]);
+
+  const ingrdientListsMeasure = Object
+    .entries(obAPI).filter((key) => key[0]
+      .includes('strMeasure'))
+    .map((recipe) => recipe[1]);
+
+  const listIngredients = [];
+  for (let i = 0; i < ingrdientLists.length; i += 1) {
+    const recipe = {
+      ingredient: ingrdientLists[i],
+      measure: ingrdientListsMeasure[i],
+    };
+    listIngredients.push(recipe);
+  }
+  return listIngredients
+    .filter((item) => item.ingredient !== ' '
+      && item.ingredient !== null
+      && item.ingredient !== '');
+}
+
+export const recipeFavoriteFactory = (obj, rota) => ({
+  id: obj.idDrink || obj.idMeal,
+  type: rota,
+  nationality: obj.strArea || '',
+  category: obj.strCategory,
+  alcoholicOrNot: obj.strAlcoholic || '',
+  name: obj.strCategory,
+  image: obj.strMealThumb || obj.strDrinkThumb,
+  doneDate: obj.dateModifield || '',
+  tags: obj.strTags || '',
+});
